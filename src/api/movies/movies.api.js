5556f2efebd142e579bc40b9851d65d4d81a2693
cherryJubilee <hyewon5026@gmail.com>
2024-02-01 10:05:08 +0900

    fix env file

--- src/api/movies/movies.api.js
+++ src/api/movies/movies.api.js
@@ -1,12 +1,12 @@
-// require("dotenv").config();
-
+const TMDB_ACCESS_TOKEN = process.env.REACT_APP_TMDB_ACCESS_TOKEN;
 const options = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}`,
+    Authorization: `Bearer ${TMDB_ACCESS_TOKEN}`,
   },
 };
+
 const getMovies = async (type) => {
   const endpoints = {
     nowPlaying:
